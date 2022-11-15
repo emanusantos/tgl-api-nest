bc843108c052a41aee12fcdf4a33c0e20932c61e
emanusantos <emanuel.lmds1@gmail.com>
2022-11-15 14:08:09 -0300

    feat: finished user crud

--- src/domains/user/user.controller.ts
+++ src/domains/user/user.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('/user')
-  getUsers() {
-    return this.userService.getUsers();
+  @Post()
+  async create(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.userService.find();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.userService.findById(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.userService.delete(id);
   }
 }
--- src/domains/user/user.service.ts
+++ src/domains/user/user.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
-  getUsers() {
-    return 'Hello users!';
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(dto: CreateUserDto) {
+    const user = new User(dto);
+
+    await this.userRepository.save(user);
+  }
+
+  async find() {
+    return this.userRepository.find();
+  }
+
+  async findById(id: string) {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async delete(id: string) {
+    const userToDelete = await this.findById(id);
+
+    if (!userToDelete) throw new NotFoundException('User not found');
+
+    await this.userRepository.remove(userToDelete);
   }
 }
